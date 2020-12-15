af52e7830c547c682fee22d9d932de2262ef9564
Jean Cedron <jean@fitco.com.pe>
2020-12-15 16:02:44 -0500

    Update GA tracking code

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,26 @@ class MyDocument extends Document {
     render() {
         return (
             <Html>
-                <Head />
+                <Head>
+                    <script
+                        async
+                        src={`https://www.googletagmanager.com/gtag/js?id=UA-76827860-2`}
+                        crossOrigin='anonymous'
+                    />
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                                window.dataLayer = window.dataLayer || [];
+                                function gtag(){dataLayer.push(arguments);}
+                                gtag('js', new Date());
+                                gtag('config', 'UA-76827860-2', {
+                                  page_path: window.location.pathname,
+                                });
+                              `,
+                        }}
+                        crossOrigin='anonymous'
+                    />
+                </Head>
                 <body>
                 <Layout>
                     <Main />
