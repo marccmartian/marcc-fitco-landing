9c644fd40d49f5e26fdec5e33f37981bd4eadfe2
Marvin Carrasco <marvincarrascoc@yahoo.com>
2021-06-10 16:47:55 -0500

    feat: edit text width

--- pages/index.js
+++ pages/index.js
@@ -36,12 +36,14 @@ export default function Home() {
                         </div>
                         <div className="info-wrapper">
                             <p className="moves-target">PARA EMPRESAS</p>
-                            <h1 className="title-normal">Asegura el <br/> bienestar de tus</h1>
+                            <h1 className="title-normal">Asegura el<br/> bienestar de tus</h1>
                             <div className="highlighted">
                                 <h1 className="title-bold">colaboradores</h1>
                                 <div className="underline"></div>
                             </div>                            
-                            <span className="text">Implementamos y gestionamos tu programa de <br/>bienestar corporativo.</span>
+                            <span className="text">
+                                Implementamos y gestionamos <br className="break-mobile"/>tu programa de <br className="break-desktop"/>bienestar corporativo.
+                            </span>
                             <a href="https://fitcolatam.com/moves">
                                 <button className="btn-moves">¡Lo quiero en mi empresa!</button>
                             </a>                    
@@ -63,11 +65,11 @@ export default function Home() {
                         <div className="info-wrapper">
                             <p className="software-target">PARA CENTROS FITNESS Y ENTRENADORES</p>
                             <div className="highlighted">
-                                <h1 className="title-bold">Transforma</h1>
+                                <h1 className="title-bold">Transforma <span className="you-mobile">tu</span></h1>
                                 <div className="underline"></div>
                             </div>
-                            <h1 className="title-normal">tu negocio fitness<br/>con nuestro software</h1>
-                            <span className="text">Haz crecer tu negocio y posiciona tu marca con un canal<br/>de ventas online, tu propia página web, una gestión<br/>sencilla y ¡mucho más!</span>
+                            <h1 className="title-normal"><span className="you-desktop">tu</span> negocio fitness<br className="break-desktop"/> con nuestro software</h1>
+                            <span className="text">Haz crecer tu negocio y posiciona tu marca con un canal<br className="break-desktop"/> de ventas online, tu propia página web, una gestión<br className="break-desktop"/> sencilla y ¡mucho más!</span>
                             <a href="https://fitcolatam.com/software">
                                 <button className="btn-software">¡Conoce nuestro software!</button>
                             </a>
